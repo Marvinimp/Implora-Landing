e4f3239b97a230d9901e1f09e125b2f07f55317e
Marvin Mews <marvin@MacBook-Air-von-Marvin.fritz.box>
2025-06-22 17:55:11 +0200

    fix base path for local + GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/Implora-Landing/',
+  base: mode === 'production' ? '/Implora-Landing/' : '/',
   server: {
     host: "::",
     port: 8080,
