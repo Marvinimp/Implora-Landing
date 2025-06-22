ef35330105d9f6ad6d5ceae6e1bef9a93d7d0f06
Marvinimp <mewsmarvin34@gmail.com>
2025-06-22 20:01:13 +0200

    Fix etc

--- src/pages/vite.config.ts
+++ src/pages/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/repo-name/',
+  base: mode === 'production' ? '/Implora-Landing/' : '/',
 
   plugins: [react()],
 });
